5e889ed0d4b3c56089154a60f6c26ab7f98dd863
Max <makcim510@mail.ru>
2020-05-16 11:39:04 -0400

    Created dashboard, added pages and a little bit of styling

--- mysite/dashboard/urls.py
+++ mysite/dashboard/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.home, name="home"),
+    path('messages/', views.myMessages, name="myMessages"),
+    path('subscriptions/', views.mySubs, name='mySubs'),
+    path('payouts/', views.myPayouts, name='myPayouts'),
+    path('support/', views.support, name='support'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index (request):
-    return HttpResponse("<h3>Hello world!</h3>")
-# Create your views here.
+def home(request):
+    return render(request, 'dashboard/home.html')
+
+def myMessages(request):
+    return render(request, 'dashboard/myMessages.html')
+
+def mySubs(request):
+    return render(request, 'dashboard/mySubs.html')
+
+def myPayouts(request):
+    return render(request, 'dashboard/myPayouts.html')
+
+def support(request):
+    return render(request, 'dashboard/support.html')
+
+def contacts(request):
+    return render(request, 'dashboard/contacts.html')

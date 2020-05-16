397f6243349c464d013b3df0413ae2d501c39c5e
Max <makcim510@mail.ru>
2020-05-16 13:32:10 -0400

    Added login_required for all dashboard pages

--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'dashboard/home.html')
 
+@login_required
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
 
+@login_required
 def myMessages(request):
     return render(request, 'dashboard/myMessages.html')
 
+@login_required
 def mySubs(request):
     return render(request, 'dashboard/mySubs.html')
 
+@login_required
 def myPayouts(request):
     return render(request, 'dashboard/myPayouts.html')
 
+@login_required
 def support(request):
     return render(request, 'dashboard/support.html')
 
+@login_required
 def contacts(request):
     return render(request, 'dashboard/contacts.html')

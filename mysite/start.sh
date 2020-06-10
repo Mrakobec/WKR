c40da852a96972fcbd29995f71fec579db75c54f
Gleb Gavrilenko <gleb.gavrilenko@gmail.com>
2020-06-10 19:39:12 +0200

    sqliteadmin configs

--- mysite/start.sh
+++ mysite/start.sh
@@ -2,6 +2,7 @@
 #
 
 docker-compose rm -sf ; docker-compose up --no-start; docker-compose start
+docker cp ./sqliteadmin/phpliteadmin.config.php mysite_phpliteadmin_1:/srv/phpliteadmin.config.php 
 echo Use docker-compose logs -f
 
 
